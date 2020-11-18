b0c9bb8f82a432c36e029891b79686e9c53e3599
victoriaehrbar <victoriaehrbar@gmail.com>
2020-11-18 22:43:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def line(deli)
+  if deli.empty?
+  puts "The line is currently empty."
+else
+ line = "The line is currently:"
+  deli.each.with_index(1) do |names, x|
+line << " #{x}. #{names}"
+end
+puts line 
+end
+end
