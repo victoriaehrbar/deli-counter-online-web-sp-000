8494576f28a49262742dbd9780bf3f6471d97c1a
victoriaehrbar <victoriaehrbar@gmail.com>
2020-11-18 23:03:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,5 +12,5 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  puts "Welcome, #{name}. You are number 1 in line."
+  puts "Welcome, #{name}. You are number {line.length} in line."
 end
